d17d574a986ebfbfec2df56eb7781959be55b4a6
vagmi <vagmi.mudumbai@gmail.com>
2011-04-09 17:24:50 +0530

    add new and create action for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,11 +3,20 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   def new
+    @restaurant = Restaurant.new
   end
   def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    if(@restaurant.save)
+      flash[:notice] = "Restaurant saved successfully"
+      redirect_to @restaurant
+    else
+      render :action=>:new
+    end
   end
   def show
     @restaurant = Restaurant.find(params[:id])
+    
   end
   def edit
   end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ActiveRecord::Base
+  validates_presence_of :name
 end
