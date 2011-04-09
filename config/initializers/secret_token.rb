e10aeaee6feb74f5d5245ef6f7c377455ba3e920
vagmi <vagmi.mudumbai@gmail.com>
2011-04-09 12:41:07 +0530

    initial version of bhojanam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BhojanamPlain::Application.config.secret_token = 'b77ca525a4114ff148d46e1f9d80cb2da881bf767c49ddbfdea87f5ad5acabb1a9f82f08b06dee8f5c677ae6dfc45b4489abbf7df446ef557707766350eee6fd'
---
+++ db/migrate/20110409071023_create_restaurants.rb
@@ -0,0 +1,15 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def self.up
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :address
+      t.text :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :restaurants
+  end
+end
