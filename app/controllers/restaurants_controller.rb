8bfd6398c5493ac305b570ec1f362b16d2dee2ab
vagmi <vagmi.mudumbai@gmail.com>
2011-04-09 12:52:01 +0530

    add the 7 actions to the restaurants controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,16 @@
 class RestaurantsController < ApplicationController
+  def index
+  end
+  def new
+  end
+  def create
+  end
+  def show
+  end
+  def edit
+  end
+  def update
+  end
+  def destroy
+  end
 end
