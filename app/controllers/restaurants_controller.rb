35fa49f144688782f53c7a47c349d2557ff11de2
vagmi <vagmi.mudumbai@gmail.com>
2011-04-09 17:56:33 +0530

    add destroy action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -31,5 +31,9 @@ class RestaurantsController < ApplicationController
     end
   end
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = "Restaurant deleted successfully."
+    redirect_to restaurants_path
   end
 end
